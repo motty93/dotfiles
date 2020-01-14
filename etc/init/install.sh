6cb6686a43b92bf719064ee4b03fe4f8084addc4
motty93 <rdwbocungelt5@gmail.com>
2020-01-14 21:01:33 +0900

    update: install.sh

---
+++ etc/init/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+packages = (
+  vim
+  curl
+  openssh
+  git
+  ufw
+  #background papers
+  mint-backgrounds-*
+  ubuntu-wallpapers-*
+  ubuntustudio-wallpapers
+  xubuntu-community-wallpapers-*
+)
